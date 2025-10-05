978e5d90fc741cf6343f8cf7730da35af7243e2c
locngodev <62205ngovanloc@gmail.com>
2025-10-05 19:27:19 +0700

    feat(backend): remote backend with Terraform Cloud

--- infrastructure_aws/provider.tf
+++ infrastructure_aws/provider.tf
@@ -5,6 +5,13 @@ terraform {
       version = "~> 5.0"
     }
   }
+  cloud { 
+    organization = "locngodev" 
+    
+    workspaces { 
+      name = "pbl4-three-tier-infrastructure" 
+    } 
+  } 
 }
 
 provider "aws" {
