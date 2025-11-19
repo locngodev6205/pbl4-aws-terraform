6c59a3f9c28a9cd795ea2c87e6fb953d8734d5ca
locngodev <62205ngovanloc@gmail.com>
2025-11-19 23:48:21 +0700

    fix: alb_app không có -> gán là

--- infrastructure_aws/client/variables.tf
+++ infrastructure_aws/client/variables.tf
@@ -37,7 +37,9 @@ locals {
 
   ec2_ecr_instance_profile_name = data.terraform_remote_state.network.outputs.ec2_ecr_instance_profile_name
 
-  alb_app_dns_name = data.terraform_remote_state.server.outputs.alb_app_dns_name
+  alb_app_dns_name = try (
+    data.terraform_remote_state.server.outputs.alb_app_dns_name, ""
+  )
 
 }
 
