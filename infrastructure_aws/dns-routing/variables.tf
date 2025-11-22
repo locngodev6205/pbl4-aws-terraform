5c9858d70268053053a080ad40c06e07c784bc10
locngodev <62205ngovanloc@gmail.com>
2025-11-22 13:01:51 +0700

    fix: add try variable

--- infrastructure_aws/dns-routing/variables.tf
+++ infrastructure_aws/dns-routing/variables.tf
@@ -30,8 +30,9 @@ variable "aws_region" {
 }
 
 locals {
-  active_alb_dns_name = var.active_color == "blue" ? data.terraform_remote_state.blue.outputs.alb_web_dns_name : data.terraform_remote_state.green.outputs.alb_web_dns_name
-  active_alb_zone_id  = var.active_color == "blue" ? data.terraform_remote_state.blue.outputs.alb_zone_web_id  : data.terraform_remote_state.green.outputs.alb_zone_web_id
+  active_alb_dns_name = var.active_color == "blue" ? try(data.terraform_remote_state.blue.outputs.alb_web_dns_name, "") : try(data.terraform_remote_state.green.outputs.alb_web_dns_name, "")
+
+  active_alb_zone_id = var.active_color == "blue" ? try(data.terraform_remote_state.blue.outputs.alb_zone_web_id, "") : try(data.terraform_remote_state.green.outputs.alb_zone_web_id, "")
 }
 
 
